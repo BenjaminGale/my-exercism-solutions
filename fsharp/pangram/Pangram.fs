2ac9dcc73dc30eea757b6b8e5c7d47358a45b939
Benjamin Gale <benjaminiangale@gmail.com>
2020-07-09 18:23:33 +0100

    Add solution: language=F#, name=Pangram

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,9 @@
+﻿module Pangram
+
+let alphabet: char Set = Set.ofSeq { 'a' .. 'z' }
+
+let isPangram (input: string): bool =
+    input
+    |> fun s -> s.ToLower()
+    |> Set.ofSeq
+    |> Set.isSubset alphabet
