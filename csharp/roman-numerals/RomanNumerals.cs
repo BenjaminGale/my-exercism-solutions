cad1bc6f3dd14f8659d4a951270fcdae0b488a0d
Benjamin Gale <benjaminiangale@gmail.com>
2024-09-15 19:06:49 +0100

    Update solution: language=C#, name=Roman Numerals

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -3,11 +3,20 @@ using System;
 public static class RomanNumeralExtension
 {
     public static string ToRoman(this int value) =>
-        ToRoman(value / 1000 % 10, "M", "", "")
-            + ToRoman(value / 100 % 10, "C", "D", "M")
-            + ToRoman(value / 10 % 10, "X", "L", "C")
-            + ToRoman(value % 10, "I", "V", "X");
+        $"{ToRomanThousands(value)}{ToRomanHundreds(value)}{ToRomanTens(value)}{ToRomanUnits(value)}";
 
+    private static string ToRomanThousands(int value) =>
+        ToRoman(value / 1000 % 10, "M", "", "");
+
+    private static string ToRomanHundreds(int value) =>
+        ToRoman(value / 100 % 10, "C", "D", "M");
+
+    private static string ToRomanTens(int value) =>
+        ToRoman(value / 10 % 10, "X", "L", "C");
+
+    private static string ToRomanUnits(int value) =>
+        ToRoman(value % 10, "I", "V", "X");
+    
     private static string ToRoman(int digit, string one, string five, string ten)
     {
         if (digit < 0 || digit > 9)
@@ -17,14 +26,14 @@ public static class RomanNumeralExtension
         {
             0 => "",
             1 => one,
-            2 => one + one,
-            3 => one + one + one,
-            4 => one + five,
-            5 => five,
-            6 => five + one,
-            7 => five + one + one,
-            8 => five + one + one + one,
-            9 => one + ten
+            2 => $"{one}{one}",
+            3 => $"{one}{one}{one}",
+            4 => $"{one}{five}",
+            5 => $"{five}",
+            6 => $"{five}{one}",
+            7 => $"{five}{one}{one}",
+            8 => $"{five}{one}{one}{one}",
+            9 => $"{one}{ten}"
         };
     }
 }
