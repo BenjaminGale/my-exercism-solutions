b5594c539eb1211f116898f358f8ef4033330d63
Benjamin Gale <benjaminiangale@gmail.com>
2024-09-16 17:20:28 +0100

    Update solution: language=Haskell, name=Leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,11 +1,7 @@
 module LeapYear (isLeapYear) where
 
--- on every year that is evenly divisible by 4
---   except every year that is evenly divisible by 100
---     unless the year is also evenly divisible by 400
-
 isLeapYear :: Integer -> Bool
-isLeapYear year = isDivisibleBy 4 year && not (isDivisibleBy 100 year) || isDivisibleBy 400 year
+isLeapYear year = isDivisibleBy 4 && not (isDivisibleBy 100) || isDivisibleBy 400
   where
-    isDivisibleBy :: Integer -> Integer -> Bool
-    isDivisibleBy divider num = num `mod` divider == 0
+    isDivisibleBy :: Integer -> Bool
+    isDivisibleBy divider = year `mod` divider == 0
