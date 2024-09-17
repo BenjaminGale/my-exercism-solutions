fa130503820bb51fbddc9958f5f214c783f47768
Benjamin Gale <benjaminiangale@gmail.com>
2024-09-17 23:26:32 +0100

    Update solution: language=Rust, name=Luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -3,15 +3,13 @@ pub fn is_valid(code: &str) -> bool {
     if code.trim().len() <= 1 { return false }
     if !code.chars().all(|c| c.is_ascii_digit() || c.is_whitespace()) { return false }
     
-    let checksum: u32 = code
+    code
         .chars()
         .filter(|c| c.is_ascii_digit())
         .rev()
         .map(|c| c.to_digit(10).unwrap())
-        .zip([1, 2].iter().cycle())
-        .map(|(digit, &multiplier)| digit * multiplier)
+        .zip([1, 2].into_iter().cycle())
+        .map(|(digit, multiplier)| digit * multiplier)
         .map(|product| if product > 9 { product - 9 } else { product })
-        .sum();
-
-    checksum % 10 == 0
+        .sum::<u32>() % 10 == 0
 }
