8ee706758ac087a0a579f9c65dff88a17fd21574
Benjamin Gale <benjaminiangale@gmail.com>
2024-09-18 21:58:34 +0100

    Add solution: language=F#, name=Leap

---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,7 @@
+﻿module Leap
+
+let isDivisible (year: int) (divisor: int): bool =
+    year % divisor = 0
+
+let leapYear (year: int): bool =
+    (isDivisible year 4) && not (isDivisible year 100) || (isDivisible year 400)
